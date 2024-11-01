d9995461493785afad9925c3817f7420cc7fa874
Aymen Jebrane <ay5jbr2023@gmail.com>
2024-11-01 12:27:46 +0100

    mise en situation

---
+++ FAQ.js
@@ -0,0 +1,58 @@
+/*const accordion = document.getElementById("accordionExample");
+
+function map (, question){
+
+const result = new Array(accordion.length);
+for (let i = 0; i <= accordion.lenght; i++){
+    
+}
+}
+
+
+
+const question = [];
+
+
+qustion.push(document.getElementById("accordionExample"));
+
+map ()
+
+}
+
+const question = [];
+
+
+
+
+
+
+const hide = document.getElementById("hide");
+
+
+
+
+
+
+
+
+
+
+function show(){
+    const x = document.getElementById ("collapseOne");
+    for(let i = 0;i ;i++ ){
+        if(){
+
+        question[i];
+    }
+}
+if (x.style.display === "none"){
+    x.style.display = "block";
+}else {
+    x.style.display = "none"
+}
+}
+*/
+window.document.getElementById("header");
+
+
+window.open()
